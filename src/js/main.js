410121254278450017f044adb1517a7988eb9a6b
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-03-13 18:56:22 -0300

    ALTERAÇÃO NO NOME DO REPOSITÓRIO, ADICIONADO ARQUIVOS '.prettierrc | js-file-donwloader.min.js', ALTERAÇÕES NO ARQUIVO 'main.js'

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const options = document.querySelector('#options').value;
-
-  const apiUrl = 'https://api.waifu.im/search';
+  const apiUrl = "https://api.waifu.im/search";
+  
+  const tags = document.querySelector("#options").value || "waifu";
   const params = {
-    included_tags: [options],
+    included_tags: [tags],
   };
 
   const queryParams = new URLSearchParams();
@@ -29,9 +29,14 @@ form.addEventListener('submit', async (event) => {
       .then((response) => response.json())
       .then((data) => data.images[0].url);
 
-    const imageElement = document.querySelector('#waifu');
-
+    const imageElement = document.querySelector("#waifu");
     imageElement.src = image;
+
+    const imageBlob = await fetch(image).then((response) => response.blob());
+
+    const downloadLink = document.querySelector("#download");
+    downloadLink.href = URL.createObjectURL(imageBlob);
+
   } catch (error) {
     console.error(`Ocorreu um erro: ${error}`);
   }
