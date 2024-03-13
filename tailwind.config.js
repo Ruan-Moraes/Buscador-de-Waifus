2641c185aaa7470cfc8ded008ce398c1691fb98a
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-03-12 22:16:15 -0300

    CORPO DA PÁGINA FEITA PARA O MOBILE

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '!050609': '#050609',
+        '!F855B1': '#F855B1',
+        '!FFC6AC': '#FFC6AC',
+        '!242424': '#242424',
+      },
+      spacing: {
+        3.5: '0.875rem',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+    },
   },
   plugins: [],
 };
