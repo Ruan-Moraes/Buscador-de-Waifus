4aefb5fd83015d8b662d3abcbc7f1e98d2f4adc2
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-03-16 10:53:53 -0300

    ALTERAÇÕES NO ARQUIVO 'main.js'

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,58 @@
 const form = document.querySelector("#form");
+const addTags = document.querySelector("#add-tag");
+
+const tagsList = new Set(['waifu']);
+
+
+addTags.addEventListener("click", () => {
+  const options = document.querySelector("#options").value;
+
+  const tags = document.querySelector("#tags");
+  const li = document.createElement("li");
+
+  li.style = "display: inline-block; padding: .25rem .25rem; background-color: #F855B1; border-radius: .5rem; font-size: .5rem; font-weight: 700; color: #fff; margin: 0.5rem 0.25rem 0;";
+  
+  if (options === "") {
+    alert("Você deve preencher o campo de tags para adicionar uma nova tag.");
+  } else {
+    tagsList.add(options);
+
+    for (const tag of tagsList) {
+      li.innerHTML = tag;
+
+      tags.appendChild(li);
+    }
+
+    for(let i = 0; i < tags.children.length; i++) {
+      tags.children[i].addEventListener("click", () => {
+        tagsList.delete(tags.children[i].innerHTML);
+        tags.removeChild(tags.children[i]);
+      });
+    }
+  }
+
+  console.log(tagsList);
+});
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   
   changingTheButtonState();
 
-  const paramsUrl = createRequest();
+  const paramsUrl = createRequest(Array.from(tagsList));
   const waifuImageURL = await getWaifu(paramsUrl);
 
-  changingTheButtonState();
+  if (verifyIfImageExists(waifuImageURL)) {
+    changingTheButtonState();
 
-  addImageToDOM(waifuImageURL);
-  addImageForDownload(waifuImageURL);
+    addImageToDOM(waifuImageURL);
+    addImageForDownload(waifuImageURL);
+  }
 });
 
-function createRequest() {
-  const preMadeTags = document.querySelector("#options").value || "waifu";
+function createRequest(tags) {
   const params = {
-    included_tags: [preMadeTags],
+    included_tags: tags,
   };
 
   const queryParams = new URLSearchParams();
@@ -31,6 +66,8 @@ function createRequest() {
       queryParams.set(key, params[key]);
     }
   }
+
+  console.log(queryParams.toString())
 
   return queryParams.toString();
 }
@@ -46,6 +83,16 @@ async function getWaifu(paramsUrl) {
 
     console.error(`Ocorreu um erro: ${error}`);
   }
+}
+
+function verifyIfImageExists(waifuImageURL) {
+  if (waifuImageURL === undefined) {
+    alert("Não foi possível encontrar uma waifu com essas tags, tente novamente.");
+
+    return false;
+  }
+
+  return true;
 }
 
 function changingTheButtonState () {
